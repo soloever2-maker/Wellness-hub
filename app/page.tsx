0cdedeb43c3fd857ebd522e54fb3c5807c0946de
soloever2-maker <soloever2@gmail.com>
2026-07-03 01:22:09 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { TopBar } from '@/components/top-bar'
+import { PrepareBanner } from '@/components/prepare-banner'
 import { NextBookingCard } from '@/components/next-booking-card'
 import { PackageCard } from '@/components/package-card'
 import { TodaysClasses } from '@/components/todays-classes'
@@ -13,6 +14,7 @@ export default function Page() {
     <main className="bg-background min-h-screen pb-24 flex flex-col">
       <TopBar />
       <div className="px-4 pt-4 pb-8 space-y-5 flex-1">
+        <PrepareBanner />
         <NextBookingCard />
         <PackageCard />
         <QuickActions />
